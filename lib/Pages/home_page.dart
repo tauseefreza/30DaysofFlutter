9e58ed4e3019ef64799cb3a574e60406b23d4752
mdtauseefreza <mdtauseefreza@users.noreply.github.com>
2021-02-22 00:15:51 +0530

    day13 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_app_1/models/catalog.dart';
 import 'package:flutter_app_1/widgets/drawer.dart';
 import 'package:flutter_app_1/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Koxa Costumes"),
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,10 +1,10 @@
 class CatalogModel {
   static final items = [
     Item(
-        id: 001,
+        id: 1,
         name: "iPhone 12 Pro",
         desc: "Apple iPhone 12th Generation",
-        price: 125000,
+        price: 999,
         color: "#33505a",
         image:
             "https://cdn.vox-cdn.com/thumbor/LVma7J9aEYF-IvbIKFaCxQ-RkdE=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/67683963/vpavic_4243_20201018_0121.0.0.jpg")
